219c04e1e6d8058daa7f22c491a6f31ebaff6c44
Jeffrey Seneff <jeffsnff@gmail.com>
2019-10-15 12:55:27 -0600

    Made the Get all command

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -4,9 +4,19 @@ const express = require('express');
 const bookRouter = express.Router();
 const Book = require('../models/books.js');
 
+// GET request
+bookRouter.get("/", (req, res, next) => {
+	Book.find((err, books) => {
+		if(err){
+			Response.status(500)
+			return next(err)
+		}
+		return res.status(200).send(books)
+	})
+})
 
 
-// post request
+// POST request
 bookRouter.post("/", (req, res, next) => {
     const newBook = new Book(req.body);
     newBook.save((err, newBook) => {
