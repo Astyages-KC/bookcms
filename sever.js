ee6e65a8e4cc0edaeb596ef524e7ef032958ffd2
Kevin Clark <naeaman@gmail.com>
2019-10-15 11:36:46 -0600

    First commit

---
+++ sever.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = exress()
+const PORT = 7000
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+//middleware for every request
+app.use(express.json())
+app.use(morgan('dev')) //gives updates when using CRUD
+
+//routes
+app.use('/bookscms', require('./routes/bookRouter.js'))
+
+//DB collection
+mongoose.connect('mongodb://localhost:27017/bookscmsdb',
+{
+    usedNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, () => console.log("Connected to BooksCMS DB"))
+
+//Global Error Handler
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+app.listen(PORT, () => {
+    console.log(`Server running on Port : ${PORT}`)
+})
