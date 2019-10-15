41a3cd2c9c15e84348c74cef3c8f53505ea807d7
Kevin Clark <naeaman@gmail.com>
2019-10-15 12:31:35 -0600

    added post to server js

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,1 +1,21 @@
 //This is where the routes go for the POST, GET, PUT, DELETE
+
+const express = require('express');
+const bookRouter = express.Router();
+const Book = require('../models/books.js');
+
+
+
+// post request
+bookRouter.post("/", (req, res, next) => {
+    const newBook = new Book(req.body);
+    newBook.save((err, newBook) => {
+        if (err) {
+            res.send(500);
+            return next(err);
+        }
+    })
+    return res.send(newBook);
+})
+
+module.exports = bookRouter
