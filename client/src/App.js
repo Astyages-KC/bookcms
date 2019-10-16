d036f630a612ea107dad8f85ae125ba7d041e17b
Kevin Clark <naeaman@gmail.com>
2019-10-16 11:53:45 -0600

    set up App.js to rec components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import BookSearch from "./booksearch/BookSearch.js";
+import BookEntry from "./bookentry/BookEntry.js";
+import NavBar from "./navbar/NavBar.js";
+import Footer from "./footer/Footer.js";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-
+      Hey it works, just uncomment what you are working on. - Kevin & Jeff
+      {/* <NavBar />
+      <Switch>
+        <Route path="/booksearch" component={BookSearch} />
+        <Route path="/bookentry" component={BookEntry} />
+      </Switch>
+      <Footer /> */}
     </div>
   );
 }
