f256fefc86eab2307266dcf51ab8b9b1df00c837
Jeffrey Seneff <jeffsnff@gmail.com>
2019-10-16 12:26:33 -0600

    boiler plate for db search

--- client/src/booksearch/BookSearch.js
+++ client/src/booksearch/BookSearch.js
@@ -1,1 +1,38 @@
+// Jeff made coded this
+import React, { Components } from 'react'
+
+class BookSearch extends Component {
+    constructor(){
+        super()
+        this.state = {
+            inventory: []
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1> Hello </h1>
+            </div>
+        )
+    }
+}
+
 // This is the code for book search
+
+
+// Axios calls
+    // search by :
+        //title
+        //subTitle
+        //author
+        //isbn
+
+
+// map over my items returned by axios call
+//then display it on the screen
+
+// have a search form
+
+
+export default BookSearch
